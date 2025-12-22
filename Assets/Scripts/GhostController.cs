efdc0fa9ca5a592763534a6208d6bef0e849a5ad
alaeddin <alaeddin@ahlab.org>
2025-12-22 23:18:27 +0900

    initial nav mesh

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostController : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public float speed =1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        if (!agent)
+        {
+            Debug.LogWarning("Failed to find NavMeshAgent");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!agent) return;
         
+        Vector3 target = Camera.main.transform.position;
+
+        agent.SetDestination(target);
+        agent.speed = speed;
     }
 
     public void KillGhost()
