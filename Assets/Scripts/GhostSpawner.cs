b7b92271dc9c0e4ddcf249926b878345c5160d00
Alaeddin NASSANI <alaeddin@ahlab.org>
2025-12-30 10:41:43 +0900

    check if position found

--- Assets/Scripts/GhostSpawner.cs
+++ Assets/Scripts/GhostSpawner.cs
@@ -1,28 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Meta.XR.MRUtilityKit;
 
 public class GhostSpawner : MonoBehaviour
 {
-   public GameObject ghostPrefab;
-   public float spawnTimer =1;
-   public float timer;
+    public GameObject ghostPrefab;
+    public float spawnTimer = 1;
+    public float timer;
 
-   void Update()
-   {
-       timer += Time.deltaTime;
-       if (timer >= spawnTimer)
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnTimer)
+        {
+            SpawnGhost();
+            timer -= spawnTimer;
+        }
+    }
+
+    public void SpawnGhost()
+    {
+        if (!MRUK.Instance || !MRUK.Instance.IsInitialized)
+        {
+            Debug.LogWarning("MRUK is not initialized.");
+            return;
+        }
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+        
+        int currentTry = 0;
+       while(currentTry < 1000)
        {
-           SpawnGhost();
-           timer -= spawnTimer;
+           bool hasFoundPosition = room.GenerateRandomPositionOnSurface(
+               MRUK.SurfaceType.VERTICAL,
+               .3f,
+               new LabelFilter(MRUKAnchor.SceneLabels.WALL_FACE),
+               out Vector3 pos,
+               out Vector3 norm
+           );
+           if (hasFoundPosition) {  
+               Vector3 position = pos + norm * -1f;
+               position.y = 0;
+               Instantiate(ghostPrefab, position, Quaternion.identity);
+               return;
+           }
+           else {
+               Debug.Log("Failed to find position. Try: " + currentTry);
+               currentTry++;
+           }
        }
-   }
 
-   public void SpawnGhost()
-   {
-       Vector3 position = Random.insideUnitSphere *3;
-       position.y = 0;
+    }
 
-       Instantiate(ghostPrefab, position, Quaternion.identity);
-   }
 }
